e800beb8935bdd6fd47e85290628c4470f19a867
Iman Adithya <121780063+ImanAdithya@users.noreply.github.com>
2023-10-10 06:38:26 +0530

    content updated

--- src/main/java/lk/ijse/spring/service/impl/ItemServiceImpl.java
+++ src/main/java/lk/ijse/spring/service/impl/ItemServiceImpl.java
@@ -1,11 +1,13 @@
 package lk.ijse.spring.service.impl;
 
+import lk.ijse.spring.dto.CustomerDTO;
 import lk.ijse.spring.dto.ItemDTO;
 import lk.ijse.spring.entity.Customer;
 import lk.ijse.spring.entity.Item;
 import lk.ijse.spring.repo.ItemRepo;
 import lk.ijse.spring.service.ItemService;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -28,21 +30,24 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public void deleteItem(String id) {
-
+        itemRepo.deleteById (id);
     }
 
     @Override
     public List<ItemDTO> getAllItem() {
-        return null;
+        List<Item> all = itemRepo.findAll ();
+        return mapper.map (all,new TypeToken<List<CustomerDTO>> (){}.getType ());
     }
 
     @Override
     public ItemDTO findItem(String id) {
-        return null;
+        Item item = itemRepo.findById (id).get ();
+        return mapper.map (item, ItemDTO.class);
     }
 
     @Override
     public void updateItem(ItemDTO c) {
-
+        Item item = mapper.map (c, Item.class);
+        itemRepo.save (item);
     }
 }
